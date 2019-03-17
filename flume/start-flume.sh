094a3286a3d578ff8f83c6356e37bf9de417e077
Salvo Nicotra <salvo.nicotra@gmail.com>
2019-03-17 18:39:32 +0100

    Flume 1.0

---
+++ flume/start-flume.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -v
+FLUME_CONF_DIR=/opt/flume/conf
+FLUME_AGENT_NAME=a1 
+
+[[ -d "${FLUME_CONF_DIR}"  ]]  || { echo "Flume config dir not mounted in /opt/flume-config";  exit 1; }
+[[ -z "${FLUME_AGENT_NAME}" ]] && { echo "FLUME_AGENT_NAME required"; exit 1; }
+
+echo "Starting flume agent : ${FLUME_AGENT_NAME}"
+
+COMMAND="flume-ng agent \
+  -c ${FLUME_CONF_DIR} \
+  -f ${FLUME_CONF_DIR}/${FLUME_CONF_FILE}\
+  -n ${FLUME_AGENT_NAME} \
+  -Dflume.root.logger=DEBUG,console
+  "
+
+${COMMAND}
