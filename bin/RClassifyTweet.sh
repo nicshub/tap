ba3254ca14cda75cb24644178ee6a89ad9d34a29
MarcoxDb <marco.xdbn@gmail.com>
2019-05-27 11:35:18 +0200

    add RClassifyTweet.sh

---
+++ bin/RClassifyTweet.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+REM Stop
+docker stop RTapTweet
+
+REM Remove previuos container 
+docker container rm RTapTweet
+
+docker build ../R/ --tag tap:RTweet
+docker run --network tap -e R_ACTION=tapclassifier -e TWEET="%*" -e MODEL="svm.rds" --name RTapTweet -it tap:RTweet
